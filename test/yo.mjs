af30bb909e2838bf381606629b710f9eab5d7c7a
Laurent Christophe <laurent.christophe.terken@gmail.com>
2025-01-02 17:18:43 +0100

    fix: flip convertion for function result

--- test/yo.mjs
+++ test/yo.mjs
@@ -1,7 +1,4 @@
-const Linvail = /** @type {import("../lib/runtime/library").Linvail} */ (
-  globalThis.Linvail
-);
-
-const xs = [];
-
-console.log(Linvail.inspect(xs.length));
+const f = function () {
+  return this;
+};
+Linvail.dir(new f());

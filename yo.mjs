20ff0fba04baf31604a42872d1883ad26ea3532b
Laurent Christophe <laurent.christophe.terken@gmail.com>
2025-01-03 13:27:11 +0100

    fix: flip at condition

--- yo.mjs
+++ yo.mjs
@@ -1,11 +1,39 @@
-const p = new Proxy(
-  { foo: 123 },
-  {
-    getOwnPropertyDescriptor: (target, key) => {
-      console.log({ target, key });
-      return Reflect.getOwnPropertyDescriptor(target, key);
-    },
-  },
-);
-
-console.log(Reflect.getOwnPropertyDescriptor(p, "foo"));
+const __ARAN_itr = __LINVAIL_INTRINSIC__;
+let __ARAN_par_this = this;
+let __ARAN_par_import = source => import(source);
+let __ARAN_par_import_meta = import.meta;
+let __ARAN_var__apply, __ARAN_var__construct, __ARAN_var__enterValue, __ARAN_var__leaveValue, __ARAN_var__enterClosure, __ARAN_var__leaveBranch, __ARAN_var__enterPrimitive, __ARAN_var__enterArgumentList, __ARAN_var__enterNewTarget, __ARAN_var__enterPlainExternalReference, __ARAN_var_f = __ARAN_itr["aran.deadzone"], __ARAN_var__w_32210 = __ARAN_itr["aran.deadzone"], __ARAN_var__r_4220 = __ARAN_itr["aran.deadzone"];
+__ARAN_var__apply = __ARAN_itr["aran.global"]["__LINVAIL_ADVICE__"]["apply"];
+__ARAN_var__construct = __ARAN_itr["aran.global"]["__LINVAIL_ADVICE__"]["construct"];
+__ARAN_var__enterValue = __ARAN_itr["aran.global"]["__LINVAIL_ADVICE__"]["enterValue"];
+__ARAN_var__leaveValue = __ARAN_itr["aran.global"]["__LINVAIL_ADVICE__"]["leaveValue"];
+__ARAN_var__enterClosure = __ARAN_itr["aran.global"]["__LINVAIL_ADVICE__"]["enterClosure"];
+__ARAN_var__leaveBranch = __ARAN_itr["aran.global"]["__LINVAIL_ADVICE__"]["leaveBranch"];
+__ARAN_var__enterPrimitive = __ARAN_itr["aran.global"]["__LINVAIL_ADVICE__"]["enterPrimitive"];
+__ARAN_var__enterArgumentList = __ARAN_itr["aran.global"]["__LINVAIL_ADVICE__"]["enterArgumentList"];
+__ARAN_var__enterNewTarget = __ARAN_itr["aran.global"]["__LINVAIL_ADVICE__"]["enterNewTarget"];
+__ARAN_var__enterPlainExternalReference = __ARAN_itr["aran.global"]["__LINVAIL_ADVICE__"]["enterPlainExternalReference"];
+__ARAN_par_this = __ARAN_var__enterPrimitive(__ARAN_par_this);
+__ARAN_par_import = __ARAN_var__enterPlainExternalReference(__ARAN_par_import);
+__ARAN_par_import_meta = __ARAN_var__enterPlainExternalReference(__ARAN_par_import_meta);
+__ARAN_var_f = __ARAN_var__enterPrimitive(__ARAN_var_f);
+__ARAN_var__w_32210 = __ARAN_var__enterPrimitive(__ARAN_var__w_32210);
+__ARAN_var__r_4220 = __ARAN_var__enterPrimitive(__ARAN_var__r_4220);
+__ARAN_var_f = (__ARAN_var__w_32210 = __ARAN_var__enterClosure(function (...__ARAN_par_function_arguments) {
+  let __ARAN_par_new_target = new.target;
+  let __ARAN_par_this = new.target ? void null : this;
+  let __ARAN_var__r_322610 = __ARAN_itr["aran.deadzone"], __ARAN_var__w_322510, __ARAN_var_arguments;
+  __ARAN_par_new_target = __ARAN_var__enterNewTarget(__ARAN_par_new_target);
+  __ARAN_par_function_arguments = __ARAN_var__enterArgumentList(__ARAN_par_function_arguments);
+  __ARAN_var__r_322610 = __ARAN_var__enterPrimitive(__ARAN_var__r_322610);
+  __ARAN_var__w_322510 = __ARAN_var__enterPrimitive(__ARAN_var__w_322510);
+  __ARAN_var_arguments = __ARAN_var__enterPrimitive(__ARAN_var_arguments);
+  __ARAN_var__leaveBranch(__ARAN_par_new_target) ? __ARAN_par_this = __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["Object.create"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [(__ARAN_var__r_322610 = __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.get"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_par_new_target, __ARAN_var__enterPrimitive("prototype")]), __ARAN_var__leaveBranch(__ARAN_var__leaveBranch(__ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.binary"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__enterPrimitive("==="), __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.unary"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__enterPrimitive("typeof"), __ARAN_var__r_322610]), __ARAN_var__enterPrimitive("object")])) ? __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.binary"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__enterPrimitive("!=="), __ARAN_var__r_322610, __ARAN_var__enterPrimitive(null)]) : __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.binary"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__enterPrimitive("==="), __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.unary"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__enterPrimitive("typeof"), __ARAN_var__r_322610]), __ARAN_var__enterPrimitive("function")])) ? __ARAN_var__r_322610 : __ARAN_var__enterValue(__ARAN_itr["Object.prototype"]))]) : void null;
+  __ARAN_var_arguments = __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.toArgumentList"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_par_function_arguments, __ARAN_var__enterPrimitive(null)]);
+  break_return: {
+    __ARAN_var__w_322510 = __ARAN_par_this;
+    break break_return;
+  }
+  return __ARAN_var__leaveBranch(__ARAN_par_new_target) ? __ARAN_var__leaveBranch(__ARAN_var__leaveBranch(__ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.binary"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__enterPrimitive("==="), __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.unary"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__enterPrimitive("typeof"), __ARAN_var__w_322510]), __ARAN_var__enterPrimitive("object")])) ? __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.binary"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__enterPrimitive("!=="), __ARAN_var__w_322510, __ARAN_var__enterPrimitive(null)]) : __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.binary"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__enterPrimitive("==="), __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.unary"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__enterPrimitive("typeof"), __ARAN_var__w_322510]), __ARAN_var__enterPrimitive("function")])) ? __ARAN_var__w_322510 : __ARAN_par_this : __ARAN_var__w_322510;
+}), __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["Reflect.defineProperty"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__w_32210, __ARAN_var__enterPrimitive("length"), __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.createObject"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__enterPrimitive(null), __ARAN_var__enterPrimitive("value"), __ARAN_var__enterPrimitive(0), __ARAN_var__enterPrimitive("writable"), __ARAN_var__enterPrimitive(false), __ARAN_var__enterPrimitive("enumerable"), __ARAN_var__enterPrimitive(false), __ARAN_var__enterPrimitive("configurable"), __ARAN_var__enterPrimitive(true)])]), __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["Reflect.defineProperty"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__w_32210, __ARAN_var__enterPrimitive("name"), __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.createObject"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__enterPrimitive(null), __ARAN_var__enterPrimitive("value"), __ARAN_var__enterPrimitive("f"), __ARAN_var__enterPrimitive("writable"), __ARAN_var__enterPrimitive(false), __ARAN_var__enterPrimitive("enumerable"), __ARAN_var__enterPrimitive(false), __ARAN_var__enterPrimitive("configurable"), __ARAN_var__enterPrimitive(true)])]), __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["Reflect.defineProperty"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__w_32210, __ARAN_var__enterPrimitive("prototype"), __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.createObject"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__enterPrimitive(null), __ARAN_var__enterPrimitive("value"), __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["Object.defineProperty"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["Object.create"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__enterValue(__ARAN_itr["Object.prototype"])]), __ARAN_var__enterPrimitive("constructor"), __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.createObject"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__enterPrimitive(null), __ARAN_var__enterPrimitive("value"), __ARAN_var__w_32210, __ARAN_var__enterPrimitive("writable"), __ARAN_var__enterPrimitive(true), __ARAN_var__enterPrimitive("enumerable"), __ARAN_var__enterPrimitive(false), __ARAN_var__enterPrimitive("configurable"), __ARAN_var__enterPrimitive(true)])]), __ARAN_var__enterPrimitive("writable"), __ARAN_var__enterPrimitive(true), __ARAN_var__enterPrimitive("enumerable"), __ARAN_var__enterPrimitive(false), __ARAN_var__enterPrimitive("configurable"), __ARAN_var__enterPrimitive(false)])]), __ARAN_var__w_32210);
+__ARAN_var__leaveValue((__ARAN_var__r_4220 = __ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.readGlobal"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__enterPrimitive("Linvail")]), __ARAN_var__apply(__ARAN_var__apply(__ARAN_var__enterValue(__ARAN_itr["aran.get"]), __ARAN_var__enterValue(__ARAN_itr["undefined"]), [__ARAN_var__r_4220, __ARAN_var__enterPrimitive("dir")]), __ARAN_var__r_4220, [__ARAN_var_f])));

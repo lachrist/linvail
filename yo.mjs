52c47c24ae5a5c7efcc49f94c1b36aa1515313d9
Laurent Christophe <laurent.christophe.terken@gmail.com>
2025-02-08 23:14:50 +0100

    fix: declare entering undefined when missing value in descriptor

--- yo.mjs
+++ yo.mjs
@@ -1,13 +1,5 @@
-const d = {
-  value: 123,
-  writable: true,
-  enumerable: true,
-  configurable: true,
-};
-const p = new Proxy(
-  {},
-  {
-    getOwnPropertyDescriptor: (target, key, descriptor) => d,
-  },
-);
-console.log(Object.getOwnPropertyDescriptor(p, "foo") === d);
+var o = { foo: 123 };
+Reflect.defineProperty(o, "foo", {
+  enumerable: false,
+});
+console.log(Reflect.getOwnPropertyDescriptor(o, "foo"));

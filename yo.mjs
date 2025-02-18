3e41935c456a4d6c97eaccb0edd1269075e081b9
Laurent Christophe <laurent.christophe.terken@gmail.com>
2025-02-18 13:26:48 +0100

    feat: add oracle for Array.prototype.sort

--- yo.mjs
+++ yo.mjs
@@ -1,19 +1,6 @@
-// const arr = {
-//   __proto__: {
-//     __proto__: null,
-//     0: undefined,
-//   },
-//   length: 3,
-//   1: "bar",
-// };
-
-// console.log(Array.prototype.map.call(arr, (...args) => args));
-
-const array = [1, 2, 3, 4, 5, 6];
-
 console.log(
-  array.map((...args) => {
-    array.pop();
-    return args;
-  }).length,
+  [4, , , , 1, , , 2, , 3].sort((...args) => {
+    console.log(args);
+    return String(args[0] - args[1]);
+  }),
 );

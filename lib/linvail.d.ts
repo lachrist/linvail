3b5068a1872f6710cf639ea7c48c319f4db768dc
Laurent Christophe <laurent.christophe.terken@gmail.com>
2025-03-04 16:05:20 +0100

    fix: re-export const in top-level type declaration

--- lib/linvail.d.ts
+++ lib/linvail.d.ts
@@ -3,6 +3,6 @@ export type * from "./instrument/node.d.ts";
 export type * from "./runtime/standard.d.ts";
 export type * from "./advice.d.ts";
 
-export type * from "./library.d.ts";
-export type * from "./runtime.d.ts";
-export type * from "./instrument.d.ts";
+export * from "./library";
+export * from "./runtime";
+export * from "./instrument";

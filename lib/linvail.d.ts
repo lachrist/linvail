cf785cc7a12449d8ee4ee0e07bf6c0b71924f01f
Laurent Christophe <laurent.christophe.terken@gmail.com>
2025-03-04 16:18:20 +0100

    fix: export library type

--- lib/linvail.d.ts
+++ lib/linvail.d.ts
@@ -1,6 +1,7 @@
 export type * from "./runtime/domain.d.ts";
-export type * from "./instrument/node.d.ts";
 export type * from "./runtime/standard.d.ts";
+export type * from "./instrument/node.d.ts";
+export type * from "./library/library.d.ts";
 export type * from "./advice.d.ts";
 
 export * from "./library";
